743e7bfe7206c72ee884657a7aa07591392e4cec
Giovanna Freitas <giovannafreitas15.gf@gmail.com>
2020-10-19 19:49:45 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,74 @@
+const cards = document.querySelectorAll('.card');
+let hasFlippedCard = false;
+let firstCard, secondCard;
+let lockBoard = false;
+
+
+// para virar a carta
+function flipCard() {
+
+    if(lockBoard) return;
+    if(this === firstCard) return;
+
+    this.classList.add('flip');
+    if(!hasFlippedCard) {
+        hasFlippedCard = true;
+        firstCard = this;
+        return;
+    }
+
+    secondCard = this;
+    hasFlippedCard = false;
+    checkForMatch();
+}
+
+// conferir se cartas são iguais
+function checkForMatch() {
+    if(firstCard.dataset.card == secondCard.dataset.card) {
+        disableCards();
+        return;
+    }
+
+    unflipCards();
+}
+
+// desabilitar cartas
+function disableCards() {
+    firstCard.removeEventListener('click', flipCard);
+    secondCard.removeEventListener('click', flipCard);
+
+    resetBoard();
+}
+
+// desvirar cartas
+function unflipCards() {
+
+    lockBoard = true
+
+    setTimeout(() => {
+        firstCard.classList.remove('flip');
+        secondCard.classList.remove('flip');
+
+        resetBoard();
+    }, 1500);
+}
+
+// resetar as cartas
+function resetBoard() {
+    [hasFlippedCard, lockBoard] = [false, false];
+    [firstCard, secondCard] = [null, null];
+}
+
+// embaralhar as cartas
+(function shuffle(){
+    cards.forEach((card) => {
+        let ramdomPosition = Math.floor(Math.random() * 12);
+        card.style.order = ramdomPosition;
+    })
+})();
+
+// evento ao clickar na carta
+cards.forEach((card) => {
+    card.addEventListener('click', flipCard)
+});
+
